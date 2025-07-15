4d4a076f2a2a821848123cdc669a7ec533dc4d7e
Happpy-said <happybargaoui@gmail.com>
2025-07-15 15:48:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import "../globals.css";
 import Navbar from "../components/Navbar";
 import WhatsAppButton from "../components/WhatsAppButton";
+import Footer from "../components/Footer";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="bg-black text-white min-h-screen">
+      <body className="bg-black text-white min-h-screen flex flex-col">
         <Navbar />
-        <main className="pt-20">{children}</main>
+        <main className="pt-20 flex-grow">{children}</main>
         <WhatsAppButton />
+        <Footer />
       </body>
     </html>
   );
